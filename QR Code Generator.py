6fc9fb81ae6a856939b1c41bc4892bd73da395a7
Ayberk Arif Kesikbaş <70178220+PioXRaV@users.noreply.github.com>
2022-07-30 22:59:13 +0300

    Add files via upload

--- QR Code Generator.py
+++ QR Code Generator.py
@@ -2,14 +2,17 @@ import qrcode
 
 data = input("Enter the Data.\n---> ")
 border = input("\nEnter the Border Thick. Recommended = 1 (0-100)\n---> ")
-f_color = input("\nEnter the Fill Color Name. Recommended = Black\n---> ")
-b_color = input("\nEnter the Back Color Name. Recommended = White\n---> ")
+f_color_hex = input("\nEnter the Fill Color as hex code. (Black = 000000)\n---> ")
+b_color_hex = input("\nEnter the Back Color as hex code. (White = FFFFFF)\n---> ")
+
+f_color = tuple(int(f_color_hex[i:i+2], 16) for i in (0, 2, 4))
+b_color = tuple(int(b_color_hex[i:i+2], 16) for i in (0, 2, 4))
 
 qr = qrcode.QRCode(version=1, error_correction=qrcode.constants.ERROR_CORRECT_L, box_size=50, border=border)
 
 qr.add_data(data)
 
-qrimg = qr.make_image(fill_color=f_color, back_color=b_color)
+qrimg = qr.make_image(fill_color=(f_color[0], f_color[1], f_color[2]), back_color=(b_color[0], b_color[1], b_color[2]))
 
 qrimg.save("QR.png")
 
